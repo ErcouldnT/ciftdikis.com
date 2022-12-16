12361152ab50bec03aa12f09d26b51971e93c590
Ercode Techoglu <ercode@pm.me>
2022-12-16 20:53:40 +0300

    ui fix: ürün onayla

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -34,44 +34,46 @@
 
 	<div class="flex flex-row justify-center gap-5">
 		<div class="w-1/2 border p-5 rounded">
-			<div class="p-2 font-bold">Ürün onaylama</div>
-			{#each $allProducts.filter((product) => {
-				return product.approved === false;
-			}) as p}
-				<div class="flex flex-row justify-center items-center gap-5 p-2">
-					<img class="rounded-xl h-32 mr-2" src={p.imgLink} alt="" />
-					<div>
-						<div>Ürün adı: {p.productName}</div>
-						<div>Satıcı: {p.seller.displayName}</div>
-						<!-- <div>İlan tarihi: {moment(Date(p.createdAt)).format('LLL')}</div> -->
-						{#if p.tags}
-							<div>Etiketler: {p?.tags?.join(', ')}</div>
-						{/if}
-					</div>
-					<div class="flex flex-col gap-2">
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<div
-							on:click={async () => {
-								await approveProduct(p.id);
-								window.location.reload();
-							}}
-							class="btn btn-warning btn-outline"
-						>
-							Onay ver
+			<div class="p-2 font-bold">Ürün onayla</div>
+			<div class="grid grid-cols-3 gap-2 m-auto">
+				{#each $allProducts.filter((product) => {
+					return product.approved === false;
+				}) as p}
+					<!-- <div class="flex flex-row justify-center items-center gap-5 p-2"> -->
+						<img class="rounded-xl h-32 mr-2 m-auto" src={p.imgLink} alt="" />
+						<div class="m-auto">
+							<div>Ürün adı: {p.productName}</div>
+							<div>Satıcı: {p.seller.displayName}</div>
+							<!-- <div>İlan tarihi: {moment(Date(p.createdAt)).format('LLL')}</div> -->
+							{#if p.tags}
+								<div>Etiketler: {p?.tags?.join(', ')}</div>
+							{/if}
 						</div>
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<div
-							on:click={async () => {
-								await removeProduct(p.id);
-								window.location.reload();
-							}}
-							class="btn btn-error btn-outline"
-						>
-							Sil
+						<div class="grid grid-cols-2 gap-2 m-auto">
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+							<div
+								on:click={async () => {
+									await approveProduct(p.id);
+									window.location.reload();
+								}}
+								class="btn btn-warning btn-outline"
+							>
+								Onay ver
+							</div>
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+							<div
+								on:click={async () => {
+									await removeProduct(p.id);
+									window.location.reload();
+								}}
+								class="btn btn-error btn-outline"
+							>
+								Sil
+							</div>
 						</div>
-					</div>
-				</div>
-			{/each}
+					<!-- </div> -->
+				{/each}
+			</div>
 		</div>
 		<div class="w-1/2 border p-5 rounded">
 			<div class="p-2 font-bold">Satıcı ekle</div>
@@ -96,21 +98,20 @@
 				<div class="grid grid-cols-2 gap-2 m-auto">
 					{#each satıcılar as satıcı}
 						<!-- <div class="grid grid-cols-2"> -->
-							<div>{satıcı.email}</div>
-							<div>
-								<div
-									on:click={async () => {
-										await removeSeller(satıcı.id);
-										window.location.reload();
-									}}
-									class="btn btn-error btn-outline"
-								>
-									Satıcıyı iptal et
-								</div>
+						<div>{satıcı.email}</div>
+						<div>
+							<div
+								on:click={async () => {
+									await removeSeller(satıcı.id);
+									window.location.reload();
+								}}
+								class="btn btn-error btn-outline"
+							>
+								Satıcıyı iptal et
 							</div>
+						</div>
 						<!-- </div> -->
 					{/each}
-
 				</div>
 			</div>
 		</div>
